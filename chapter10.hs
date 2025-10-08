8ee6020fd83f4e94cfd81eef1303448d5e5046ad
HulisaniDev <muravhahulisani17@gmail.com>
2025-10-08 14:46:17 +0200

    Update chapter10.hs

--- chapter10.hs
+++ chapter10.hs
@@ -1,79 +1,164 @@
---- by Hulisani Muravha 
-module Main where
--- 1. type synonyms
-type Name = String
-type Age  = Int
+{-# LANGUAGE FlexibleInstances #-}
 
--- 2. simple Person ADT
-data Person = Person Name Age
-  deriving (Show, Eq)
+-- HC10T1: ShowSimple Type Class
+class ShowSimple a where
+    showSimple :: a -> String
 
--- 3. Maybe-like custom type (for demonstration)
-data MyMaybe a = MyNothing | MyJust a
-  deriving (Show, Eq)
+data PaymentMethod = Cash | Card | Crypto
 
--- 4. Either-like custom type
-data MyEither a b = MyLeft a | MyRight b
-  deriving (Show, Eq)
+instance ShowSimple PaymentMethod where
+    showSimple Cash = "Cash"
+    showSimple Card = "Card"
+    showSimple Crypto = "Crypto"
 
--- 5. functions on Person
-isAdult :: Person -> Bool
-isAdult (Person _ age) = age >= 18
+-- HC10T2: Summable Type Class
+class Summable a where
+    sumUp :: [a] -> a
 
-greetPerson :: Person -> String
-greetPerson (Person name _) = "Hi, " ++ name ++ "!"
+instance Summable Int where
+    sumUp = sum
 
--- 6. converting between MyMaybe and Maybe
-toMaybe :: MyMaybe a -> Maybe a
-toMaybe MyNothing  = Nothing
-toMaybe (MyJust x) = Just x
+-- HC10T3: Comparable Type Class
+class Comparable a where
+    compareWith :: a -> a -> Ordering
 
-fromMaybe' :: Maybe a -> MyMaybe a
-fromMaybe' Nothing  = MyNothing
-fromMaybe' (Just x) = MyJust x
+data Blockchain = Bitcoin | Ethereum | Solana
 
--- 7. operate on MyEither
-eitherToTuple :: MyEither a b -> (Maybe a, Maybe b)
-eitherToTuple (MyLeft a)  = (Just a, Nothing)
-eitherToTuple (MyRight b) = (Nothing, Just b)
+instance Comparable Blockchain where
+    compareWith Bitcoin Bitcoin = EQ
+    compareWith Ethereum Ethereum = EQ
+    compareWith Solana Solana = EQ
+    compareWith Bitcoin _ = GT
+    compareWith Ethereum Bitcoin = LT
+    compareWith Ethereum Solana = GT
+    compareWith Solana _ = LT
 
--- 8. recursive ADT: binary tree
+-- HC10T4: Eq Instance for Box
+data Box a = Box a
 
-data MyTree a = MyEmpty | MyNode a (MyTree a) (MyTree a)
-  deriving (Show, Eq)
+instance Eq a => Eq (Box a) where
+    (Box x) == (Box y) = x == y
 
-treeSize :: MyTree a -> Int
-treeSize MyEmpty = 0
-treeSize (MyNode _ l r) = 1 + treeSize l + treeSize r
+-- HC10T5: ShowDetailed Type Class
+class ShowDetailed a where
+    showDetailed :: a -> String
 
-treeInsert :: (Ord a) => a -> MyTree a -> MyTree a
-treeInsert x MyEmpty = MyNode x MyEmpty MyEmpty
-treeInsert x (MyNode v l r)
-  | x == v    = MyNode v l r
-  | x < v     = MyNode v (treeInsert x l) r
-  | otherwise = MyNode v l (treeInsert x r)
+data User = User 
+    { userName :: String
+    , userAge :: Int
+    , userEmail :: String
+    }
 
--- main demonstrates ADTs and type synonyms:
+instance ShowDetailed User where
+    showDetailed user = "User: " ++ userName user ++ 
+                       ", Age: " ++ show (userAge user) ++ 
+                       ", Email: " ++ userEmail user
+
+-- HC10T6: Mutual Recursion in Eq for Blockchain
+instance Eq Blockchain where
+    Bitcoin == Bitcoin = True
+    Ethereum == Ethereum = True
+    Solana == Solana = True
+    _ == _ = False
+    
+    x /= y = not (x == y)
+
+-- HC10T7: Convertible Type Class
+class Convertible a b where
+    convert :: a -> b
+
+instance Convertible PaymentMethod String where
+    convert Cash = "Cash Payment"
+    convert Card = "Card Payment" 
+    convert Crypto = "Crypto Payment"
+
+-- HC10T8: AdvancedEq Subclass of Eq
+class Eq a => AdvancedEq a where
+    compareEquality :: a -> a -> Bool
+
+instance AdvancedEq Blockchain where
+    compareEquality x y = x == y
+
+-- HC10T9: MinMax Type Class
+class MinMax a where
+    minValue :: a
+    maxValue :: a
+
+instance MinMax Int where
+    minValue = minBound
+    maxValue = maxBound
+
+-- HC10T10: Concatenatable Type Class
+class Concatenatable a where
+    concatWith :: a -> a -> a
+
+instance Concatenatable [Char] where
+    concatWith = (++)
+
+-- Show instances for printing
+instance Show PaymentMethod where
+    show = showSimple
+
+instance Show Blockchain where
+    show Bitcoin = "Bitcoin"
+    show Ethereum = "Ethereum" 
+    show Solana = "Solana"
+
+instance Show User where
+    show user = "User " ++ userName user
+
+instance Show a => Show (Box a) where
+    show (Box x) = "Box " ++ show x
+
+-- Test functions to demonstrate usage
 main :: IO ()
 main = do
-  let p = Person "Hulisani" 23  -- type synonym Name used via Person constructor
-  putStrLn "Chapter 10: Types & ADTs demo"
-  putStrLn $ "Person: " ++ show p
-  putStrLn $ "isAdult: " ++ show (isAdult p)
-  putStrLn $ "greetPerson: " ++ greetPerson p
-
-  -- MyMaybe demo
-  let mm1 = MyJust 5
-      mm2 = MyNothing :: MyMaybe Int
-  putStrLn $ "toMaybe (MyJust 5): " ++ show (toMaybe mm1)
-  putStrLn $ "toMaybe MyNothing: " ++ show (toMaybe mm2)
-
-  -- MyEither demo
-  putStrLn $ "eitherToTuple (MyLeft \"err\"): " ++ show (eitherToTuple (MyLeft "err"))
-  putStrLn $ "eitherToTuple (MyRight 42): " ++ show (eitherToTuple (MyRight 42))
-
-  -- MyTree demo
-  let values = [7,3,9,1,5]
-      t = foldr treeInsert MyEmpty values
-  putStrLn $ "Tree built from " ++ show values ++ ": " ++ show t
-  putStrLn $ "treeSize: " ++ show (treeSize t)
+    putStrLn "HC10T1:"
+    print $ showSimple Cash
+    print $ showSimple Card
+    print $ showSimple Crypto
+    putStrLn ""
+    
+    putStrLn "HC10T2:"
+    print $ sumUp ([1, 2, 3, 4] :: [Int])
+    putStrLn ""
+    
+    putStrLn "HC10T3:"
+    print $ compareWith Bitcoin Ethereum
+    print $ compareWith Ethereum Solana
+    print $ compareWith Solana Solana
+    putStrLn ""  
+    
+    putStrLn "HC10T4:"
+    print $ Box (5 :: Int) == Box (5 :: Int)
+    print $ Box (5 :: Int) == Box (6 :: Int)
+    putStrLn ""    
+    
+    putStrLn "HC10T5:"
+    let testUser = User "John Doe" 30 "john@example.com"
+    putStrLn $ showDetailed testUser
+    putStrLn "" 
+    
+    putStrLn "HC10T6:"
+    print $ Bitcoin == Bitcoin
+    print $ Bitcoin /= Ethereum
+    putStrLn "" 
+    
+    putStrLn "HC10T7:"
+    putStrLn $ convert Cash
+    putStrLn $ convert Crypto
+    putStrLn "" 
+    
+    putStrLn "HC10T8:"
+    print $ compareEquality Bitcoin Bitcoin
+    print $ compareEquality Bitcoin Ethereum
+    putStrLn "" 
+    
+    putStrLn "HC10T9: MinMax"
+    print $ (minValue :: Int)
+    print $ (maxValue :: Int)
+    putStrLn "" 
+    
+    putStrLn "HC10T10:"
+    putStrLn $ concatWith "Hello " "World!"
+   
