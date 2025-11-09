c49992a62869a49892e4fdc440648f832eb4bbff
HulisaniDev <muravhahulisani17@gmail.com>
2025-11-09 17:58:30 +0200

    Update chapter11.hs

--- chapter11.hs
+++ chapter11.hs
@@ -1,39 +1,32 @@
---By Hulisani Muravha 
--- Chapter11IO.hs
--- Haskell Chapter 11 Practical Tasks (HC11T1–HC11T10)
+---by Hulisani Muravha 
+---Chapter11IO.hs
 
 import Data.Char (toUpper)
 
-------------------------------------------------------------
 -- HC11T1: Greet the User
-------------------------------------------------------------
 main1 :: IO ()
 main1 = do
   putStrLn "What is your name?"
   name <- getLine
   putStrLn ("Hello, " ++ name ++ "!")
-
-------------------------------------------------------------
+  
 -- HC11T2: Count Characters in a Line
-------------------------------------------------------------
 main2 :: IO ()
 main2 = do
   putStrLn "Enter a line:"
   line <- getLine
   putStrLn ("Number of characters: " ++ show (length line))
-------------------------------------------------------------
+  
 -- HC11T3: Double a Number
-------------------------------------------------------------
 main3 :: IO ()
 main3 = do
   putStrLn "Enter a number:"
   input <- getLine
   let number = read input :: Int
   print (number * 2)
-
-------------------------------------------------------------
+  
 -- HC11T4: Concatenate Two Lines
-------------------------------------------------------------
+
 main4 :: IO ()
 main4 = do
   putStrLn "Enter the first line:"
@@ -41,9 +34,8 @@ main4 = do
   putStrLn "Enter the second line:"
   line2 <- getLine
   putStrLn ("Combined: " ++ line1 ++ line2)
-------------------------------------------------------------
+  
 -- HC11T5: Repeat Until "quit"
-------------------------------------------------------------
 main5 :: IO ()
 main5 = do
   putStrLn "Type something (or 'quit' to stop):"
@@ -57,17 +49,15 @@ main5 = do
           putStrLn ("You typed: " ++ input)
           loop
 
-------------------------------------------------------------
+
 -- HC11T6: Uppercase Converter
-------------------------------------------------------------
 main6 :: IO ()
 main6 = do
   putStrLn "Enter text:"
   line <- getLine
   putStrLn (map toUpper line)
-------------------------------------------------------------
+  
 -- HC11T7: User Options
-------------------------------------------------------------
 main7 :: IO ()
 main7 = do
   putStrLn "Choose an option:"
@@ -81,9 +71,8 @@ main7 = do
     "3" -> putStrLn "Exiting..."
     _   -> putStrLn "Invalid option."
 
-------------------------------------------------------------
+
 -- HC11T8: Even or Odd Checker
-------------------------------------------------------------
 main8 :: IO ()
 main8 = do
   putStrLn "Enter a number:"
@@ -92,9 +81,9 @@ main8 = do
   if even n
     then putStrLn "That number is even."
     else putStrLn "That number is odd."
-------------------------------------------------------------
+
+    
 -- HC11T9: Sum Two Numbers
-------------------------------------------------------------
 main9 :: IO ()
 main9 = do
   putStrLn "Enter first number:"
@@ -104,9 +93,8 @@ main9 = do
   let sumResult = (read a :: Int) + (read b :: Int)
   putStrLn ("Sum = " ++ show sumResult)
 
-------------------------------------------------------------
+
 -- HC11T10: Reverse User Input
-------------------------------------------------------------
 main10 :: IO ()
 main10 = do
   putStrLn "Enter text to reverse:"
